f70a85bd313b401c9821193340467ddfdc6f2f1e
Joseph Surla <josephsurla@gmail.com>
2018-02-09 22:14:35 -0800

    css module created for song component

--- src/components/Song/index.js
+++ src/components/Song/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import styles from './song.module.css'
+
 const Song = () => {
   return (
-    <div>
+    <div className={styles.center}>
       <h3>Song Title</h3>
       <p>Artist Name</p>
     </div>
