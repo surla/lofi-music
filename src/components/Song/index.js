7dcc5a7b8cb4974d61fef4402ce6c4d0e368454b
Joseph Surla <josephsurla@gmail.com>
2018-02-15 13:45:13 -0800

    Include image to Song component

--- src/components/Song/index.js
+++ src/components/Song/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import styles from './song.module.css'
+import image from '../../pages/images/1.jpg'
 
 const Song = (props) => {
   return (
     <div className={styles.center}>
+      <img src={image} className={styles.image}/>
       <h4>{props.artist}</h4>
-      <h2>{props.song}</h2>
+      <h2 className={styles.song}>{props.song}</h2>
     </div>
   )
 }
